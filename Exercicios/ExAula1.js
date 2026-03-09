8cc148c12f2902dcf728fd546fc2c9114c21667c
Pedro Lima <pedrocardosolima20@gmail.com>
2026-03-09 13:47:45 -0300

    update

--- Exercicios/ExAula1.js
+++ Exercicios/ExAula1.js
@@ -2,8 +2,6 @@
 //let num1 = 4
 //let num2 = 3
 
-//console.log(num1%num2);
-
 //let soma = num1+num2
 //let sub = num1-num2
 //let div = num1/num2
@@ -11,17 +9,18 @@
 
 //console.log(`Soma = ${soma} Subtração =${sub} Divisão =${div} Multiplicação=${mult}`)
 
-//Exe -2
-//for (let index = 0; index < 100; index++) {
-//   if (index%2==1) {
-//console.log(index);
-//   }
-//}
+//Exe - 2;
+/*for (let index = 0; index < 100; index++) {
+  if (index % 2 == 1) {
+    console.log(index);
+  }
+}*/
 
 //Exe -3
-//let palavra = "Disciplina de programação para web";
+/*let palavra = "Disciplina de programação para web";
 
-//console.log(`Quantidade de caracteres ${palavra.length}`);
+console.log(`Quantidade de caracteres ${palavra.length}`);*/
+
 //Exe -4
 //let nome = "Pedro Cardoso Lima"
 //let lista = nome.split(' ')
@@ -31,15 +30,31 @@
 //------------------------------
 
 //Exe -5
-//for (let index = 0; index <= 10; index++) {
-//    if (index<= 9) {
-//        console.log(index + "-");
-//    }else{
-//        console.log(index);
-//    }    
-//}
+/*for (let index = 0; index <= 10; index++) {
+    if (index<= 9) {
+        console.log(index + "-");
+    }else{
+       console.log(index);
+    }
+}*/
 
 //Exe -6
-for (let index = 0; index < array.length; index++) {
+
+/*function dobro(a) {
+    if (a<=0) {
+        return "So e aceito numeros positivos maiores que zero"
+    }
+    return a+a
+}
+console.log(dobro(3))*/
+
+//ex -7
+
+
+//ex -8
+
+function vogais(a) {
+
     
+    return num
 }
