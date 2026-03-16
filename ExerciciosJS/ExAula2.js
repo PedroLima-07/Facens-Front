e5f10401bfd91206aeb8b9cec3e7940efe1f454b
Pedro Lima <pedrocardosolima20@gmail.com>
2026-03-16 17:04:39 -0300

    update

--- ExerciciosJS/ExAula2.js
+++ ExerciciosJS/ExAula2.js
@@ -13,9 +13,28 @@
 
 //2
 // O .replace() em JavaScript é um método usado para substituir uma parte de uma string por outra.
-function convert(num1) {
-  if (!Number.isInteger(num1)) {
-    console.log(`R$${num1.toFixed(2).replace(".", ",")}`);
-  }
-}
-convert(15.5676);
+
+// function convert(num1) {
+//   if (!Number.isInteger(num1)) {
+//     console.log(`R$${num1.toFixed(2).replace(".", ",")}`);
+//   }
+// }
+// convert(15.5676);
+
+//3
+
+//4
+
+// function divisivel(number) {
+//   if (number % 3 == 0) {
+//     return "True";
+//   } else {
+//     return "False";
+//   }
+// }
+// console.log(divisivel(1));
+
+//5
+let listNum = {};
+
+function lista(number) {}
